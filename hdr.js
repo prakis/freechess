159b6ba06ad1c52201227cd88bf9a0980b18e6b1
Krishna Pagadala <prakis@gmail.com>
2026-05-01 13:03:10 +0530

    moving to github

---
+++ hdr.js
@@ -0,0 +1,8 @@
+  function isMobile(){
+    // if we want a more complete list use this: http://detectmobilebrowsers.com/
+    // str.test() is more efficent than str.match()
+    // remember str.test is case sensitive
+    var isMobile = (/iphone|ipod|ipad|android|ie|blackberry|fennec/).test
+         (navigator.userAgent.toLowerCase());
+    return isMobile;
+}
